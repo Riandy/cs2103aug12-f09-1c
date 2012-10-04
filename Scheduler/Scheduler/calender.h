9989db47b970573f8204cc7d64e73193a5be14d9
John <John@John-PC>
2012-10-04 19:18:25 +0800

    Completed the displayDatabase function command in scheduler class, slight refactoring, completed the return to Seample class, added error checking and return strings for error cases. Added new function called generalError() for errors which should have been caught by Intellisense such as lack of input, bad input etc. For now, database errors return a static string.

--- Scheduler/Scheduler/calender.h
+++ Scheduler/Scheduler/calender.h
@@ -10,16 +10,6 @@
 
 using namespace std;
 
-//struct task
-//{
-//	string _description;
-//	tm _startDate;
-//	tm _endDate;
-//	string _priority;	
-//	string _category;
-//	int _id;
-//};
-
 class calender
 {
 public:
@@ -28,8 +18,6 @@ private:
 
 	int _numberTasks;
 	vector<task> _storage;
-	//ifstream infile;
-	//ofstream outfile;
 
 	bool writeFile();
 	bool loadFile();
@@ -40,24 +28,13 @@ public:
 calender();
 ~calender();
 
-bool addItem(task currentTask); //done
-bool deleteItem(int taskID); //done
-bool searchID(int id); //done
-bool editTask(task edited); //do later
-vector<task> SearchByCat(string searchItem); //done
-vector<task> SearchByTask(string searchItem); //done
-
-vector<task> displayDatabase(); //done
-
-
-
-
-//using a void here, because bool won't cut it, and I think we might need to think this 
-//through.
-
-void execute(string command, task currentTask);
-
-
+bool addItem(task currentTask); 
+bool deleteItem(int taskID); 
+bool checkID(int id); 
+bool editTask(task edited); 
+vector<task> SearchByCat(string searchItem); 
+vector<task> SearchByTask(string searchItem); 
+vector<task> displayDatabase(); 
 
 };
 #endif
