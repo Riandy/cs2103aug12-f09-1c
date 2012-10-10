77d19aaa9865340465006f3e7e06fc4bd84549b0
Beans_admin <Beans_admin@Beans_admin-PC>
2012-10-09 21:05:53 -0700

    fixed bug, feedback output now correct

--- SeamPLE/seample.cpp
+++ SeamPLE/seample.cpp
@@ -1,5 +1,30 @@
 #include "seample.h"
 
+
+void DisplayDate(tm date)
+{
+    cout<<string("Date : ")<<date.tm_mday;
+    cout<<string("/")<<date.tm_mon;
+    cout<<string("/")<<date.tm_year;
+    cout<<string(" , ");
+    cout<<date.tm_hour<<string(":")<<date.tm_min<<endl;
+}
+
+
+void DisplayEvent(Action task)
+{
+    cout<<string("******************")<<endl;
+    cout<<string("Command : ")<<task.getCommand()<<endl;
+    DisplayDate(task.getStartDate());
+    DisplayDate(task.getEndDate());
+    cout<<string("Priority : ")<<task.getPriority()<<endl;
+    cout<<string("Event Name : ")<<task.getEventName()<<endl;
+    cout<<string("Category : ")<<task.getCategory()<<endl;
+    //cout<<string("Req Met : ")<<task.getrequirementsMet()<<endl;
+    cout<<string("******************")<<endl;
+}
+
+
 Seample::Seample()
 {
 
@@ -30,8 +55,9 @@ void Seample::init(int argc, char *argv[])
       response=intellisense.check(userInput);
       feedback.push_back(QString::fromUtf8(intellisense.getParameter().c_str()));
       feedback.push_back("");
-      qDebug()<<feedback[1];
-      cout<<userInput<<endl;
+      qDebug()<<feedback[0];
+      DisplayEvent(response);
       _view->feedback(feedback[0]);
+      feedback.clear();
 
   }
