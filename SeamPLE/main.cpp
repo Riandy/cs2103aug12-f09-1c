1244e2cbcc6cbd6233e29e37ecb8b610e03dd7f8
Beans_admin <Beans_admin@Beans_admin-PC>
2012-10-09 21:05:53 -0700

    fixed bug, feedback output now correct

---
+++ SeamPLE/main.cpp
@@ -0,0 +1,14 @@
+#include <QtGui/QApplication>
+#include "GuiControl.h"
+#include "seample.h"
+
+int main(int argc, char *argv[])
+{
+    static Seample app;
+    app.init(argc,argv);
+
+
+
+
+    return 0;
+}
