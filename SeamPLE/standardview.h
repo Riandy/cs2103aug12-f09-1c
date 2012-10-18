76aeab1bb7dd21e6701450eea6a8c665dbb6a68f
samsung <samsung@WY>
2012-10-19 02:57:14 +0800

    Gui Update: added table view capacity

--- SeamPLE/standardview.h
+++ SeamPLE/standardview.h
@@ -3,6 +3,7 @@
 
 #include <QVector>
 #include <QString>
+#include <QTableWidgetItem>
 #include "GuiShortcuts.h"
 #include "SLineEdit.h"
 
@@ -13,6 +14,15 @@ class StandardView;
 class StandardView : public QMainWindow
 {
     Q_OBJECT
+
+private:
+    //New struct for holding the table contents
+    struct TableListNode{
+        QTableWidgetItem index;
+        QTableWidgetItem content;
+        TableListNode *prev;
+        TableListNode *next;
+    };
     
 public:
     explicit StandardView(QWidget *parent = 0);
@@ -43,9 +53,15 @@ private slots:
 private:
     void changeAutoResolution();
 
+    void addTableContent(TableListNode *curr);
+
+    void resetTableContents();
+
     Ui::StandardView *ui;
 
     GuiShortcuts _allShortcuts;
+
+    TableListNode *_tail;
 };
 
 #endif // STANDARDVIEW_H
