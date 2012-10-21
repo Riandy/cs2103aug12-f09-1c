0e1a8184df7920a2ae3c3ba68bfd92714ba8c386
samsung <samsung@WY>
2012-10-22 00:51:30 +0800

    Gui Update: added files CommonView class that is required for gui interfaces             to run

---
+++ SeamPLE/CommonView.cpp
@@ -0,0 +1,18 @@
+#include "CommonView.h"
+
+const QString CommonView::STYLESHEET_INPUT_LINE_BORDER_RADIUS =
+        "border-radius: 10px;";
+const QString CommonView::STYLESHEET_INPUT_LINE_BORDER_STYLE =
+        "border-style: ridge;";
+const QString CommonView::STYLESHEET_INPUT_LINE_BORDER_WIDTH =
+        "border-width: 4px;";
+const QString CommonView::STYLESHEET_INPUT_LINE_BORDER_COLOR =
+        "border-color: rgb(84, 84, 84);";
+const QString CommonView::STYLESHEET_INPUT_LINE_BACKGROUND_COLOR =
+        "background-color: rgb(170, 85, 0);";
+
+CommonView::CommonView()
+{
+
+
+}
