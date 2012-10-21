ef93e6ad7ccde97073c63471120e804c4aa5e3d7
Riandy <Riandy@Riandy-PC>
2012-10-21 13:57:33 +0800

    fixed the search by event name by adding the npos delete the unecessary code in the execute command

--- SeamPLE/scheduler.cpp
+++ SeamPLE/scheduler.cpp
@@ -44,82 +44,6 @@ vector<string> scheduler::executeCommand(Action newAction)
     newTask.setID(newAction.getID());
     // Switch instead of using if cases.
 
-    /*switch (command){
-
-    case (ADD):
-            {
-                eventCalender.addItem(newTask);
-                _result.push_back(ADD_SUCCESS);
-            }
-
-    case (DELETE):
-            {
-                if(eventCalender.checkID(newTask.getID()))
-                {
-                    eventCalender.deleteItem(newTask.getID());
-                    _result.push_back(DELETE_SUCCESS);
-                }
-                else
-                {
-                    _result.push_back(ERROR_NOT_FOUND);
-                }
-            }
-    case (EDIT):
-            {
-                //not yet implemented
-            }
-
-    case (FIND):
-            {
-                //case 1: search by category
-                if(newTask.getCategory()!="")
-                {
-                    taskVector = eventCalender.SearchByCat(newTask.getCategory());
-                    convertToString(taskVector);
-                }
-                //case 2: search by task
-                else if (newTask.getEventName()!="")
-                {
-                    taskVector = eventCalender.SearchByTask(newTask.getEventName());
-                    convertToString(taskVector);
-
-                }
-                else
-                    generalError();
-            }
-    case (DISPLAY):
-            {
-                taskVector = eventCalender.displayDatabase();
-                convertToString(taskVector);
-            }
-
-    case (UNDO):
-            {
-                if (eventCalender.undoAction())
-                    _result.push_back(UNDO_SUCCESS);
-                else
-                    _result.push_back(UNDO_FAILURE);
-            }
-    case (REDO):
-            {
-                if (eventCalender.redoAction())
-                    _result.push_back(REDO_SUCCESS);
-                else
-                    _result.push_back(REDO_FAILURE);
-            }
-
-    case(TODAY):
-            {
-                taskVector = eventCalender.getToday();
-                convertToString(taskVector);
-            }
-    case default:
-                 generalError();
-
-    }
-    */
-
-
     if(command=="ADD")
     {
         eventCalender.addItem(newTask);
@@ -148,8 +72,9 @@ vector<string> scheduler::executeCommand(Action newAction)
     else if(command=="FIND")
     {
         //case 1: search by category
-        if(newTask.getCategory()!="")
+        if(newTask.getCategory()!="#")
         {
+            cout<<"THIS is called"<<endl;
             taskVector = eventCalender.SearchByCat(newTask.getCategory());
             convertToString(taskVector);
         }
