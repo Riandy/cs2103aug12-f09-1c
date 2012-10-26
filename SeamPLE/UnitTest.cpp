f4dec5e31e4b7a8847674cd7c70b027a8021f1fb
Beans_admin <Beans_admin@Beans_admin-PC.mshome.net>
2012-10-26 00:30:27 -0700

    include unit test files

---
+++ SeamPLE/UnitTest.cpp
@@ -0,0 +1,12 @@
+#include "UnitTest.h"
+
+void UnitTest::dummyTest() {
+    // Always succeeds
+    QVERIFY(true);
+}
+
+
+void UnitTest::dummyTest1() {
+    // Always succeeds
+    QVERIFY(false);
+}
