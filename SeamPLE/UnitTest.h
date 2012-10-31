c4a4ca237bb8deea4d774437619b62ddf0415c7f
pwr <a0083711@nus.edu.sg>
2012-11-01 01:34:15 +0800

    Assertions added for Intellisense

---
+++ SeamPLE/UnitTest.h
@@ -0,0 +1,48 @@
+#ifndef UNITTEST_H
+#define UNITTEST_H
+
+#include <QTtest/QTtest>
+#include "intellisense.h"
+#include "task.h"
+
+class UnitTest: public QObject
+{ Q_OBJECT
+
+private slots:
+    void dummyTest();
+    void dummyTest1();
+    void testIntellisense();
+
+    void testIntellisenseAdd();
+    void testIntellisenseAdd2();
+    void testIntellisenseAdd3();
+    void testIntellisenseAdd4();
+    void testIntellisenseAdd5();
+
+    void testIntellisenseDel();
+    void testIntellisenseDel2();
+   //void testIntellisenseDEL3();
+
+    void testIntellisenseEdit();
+    void testIntellisenseEdit2();
+    void testIntellisenseExit();
+
+    void testIntellisenseDisplay();
+
+    void testIntellisenseMark();
+
+    void testIntellisenseQuickAdd();
+
+    void testIntellisenseSort();
+
+    void testIntellisenseFind();
+
+    void testIntellisenseUndo();
+
+    void testIntellisenseRedo();
+
+
+
+};
+
+#endif // UNITTEST_H
