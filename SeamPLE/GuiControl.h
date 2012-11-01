c46735f70bb4c1f0a88e73686b4bf0ed2098c116
Riandy <Riandy@Riandy-PC.nus.edu.sg>
2012-11-01 11:31:15 +0800

    fixed the find by category command

---
+++ SeamPLE/GuiControl.h
@@ -0,0 +1,104 @@
+#ifndef GUICONTROL_H
+#define GUICONTROL_H
+
+#include "NotifyView.h"
+#include "SeampleView.h"
+#include "StandardView.h"
+#include "seample.h"
+#include "timekeeper.h"
+
+class GuiControl: public QObject
+{
+    Q_OBJECT
+
+private:
+    static GuiControl* _guiControl;
+
+    const static QString MESSAGE_AVAILABLE_COMMANDS;
+    const static QString MESSAGE_INTELLISENSE_INVALID_RETURN;
+    const static QString MESSAGE_INVALID_COLOUR_FLAG_RETURN;
+    const static QString MESSAGE_SCHEDULER_INVALID_RETURN;
+
+private:
+    GuiControl();
+
+    ~GuiControl();
+
+public:
+    GuiControl* getInstance();
+
+    void endInstance();
+
+    void showGui();
+
+    void setStandardViewFlag (bool flag);
+
+
+
+private slots:
+    void check(QString input);
+
+    void passScheduler(QString input, bool inputBarHasFocus);
+
+    void changeView(QString input, QString inputChecked, bool inputBarHasFocus);
+
+    void showHideView();
+
+private:
+    bool singleInstanceExists();
+
+    bool implementInputColorFlagFailure(QCharRef colorFlag);
+
+    QVector <QString> getTodaysEvents();
+
+    void toShow(QVector <QString> events);
+
+    bool interfaceIsStandardView();
+
+    bool interfaceIsCurrentlyShown();
+
+    void setInterfaceShownFlag(bool flag);
+
+    void emptyResponse();
+
+    void send(QString feedback);
+
+    void sendWithInputEditItem (QString input, QString feedback);
+
+    void sendWithInputEditAndFocus (bool inputBarHasFocus, QString input, QString feedback);
+
+    void setStandardGuiSignals();
+
+    void setSeampleGuiSignals();
+
+    //void setGlobalSignals();
+
+    void createSystemTrayIconIfPossible();
+
+
+private:
+
+    bool _standardViewFlag;
+
+    bool _interfaceShownFlag;
+
+    InputBarFlag _inputColorFlag;
+
+    //Control class that controls the input sent in and send the appropriate
+    //results back to be printed
+    Seample *_inputProcessor;
+
+    Timekeeper _timeControl;
+
+    //GuiShortcuts _allShortcuts;
+
+    //GUI interfaces ==================
+    SeampleView* _seampleGui;
+    StandardView* _standardGui;
+    NotifyView* _notifyInterface;
+    //=================================
+
+
+
+};
+#endif // GUICONTROL_H
---
+++ SeamPLE/standardview.h
@@ -0,0 +1,124 @@
+#ifndef STANDARDVIEW_H
+#define STANDARDVIEW_H
+
+#include <QVector>
+#include <QTableWidgetItem>
+#include <QTimer>
+#include "CommonView.h"
+
+namespace Ui {
+class StandardView;
+}
+
+class StandardView : public QMainWindow, public CommonView
+{
+    Q_OBJECT
+
+private:
+    //New struct for holding the table contents
+    struct TableListNode{
+        QTableWidgetItem index;
+        QTableWidgetItem content;
+        TableListNode *prev;
+        TableListNode *next;
+    };
+
+    static StandardView* _standardView;
+
+    const static QString MESSAGE_NO_CURRENT_RESULTS;
+    
+private:
+    explicit StandardView(QWidget *parent = 0);
+    ~StandardView();
+
+public:
+    StandardView* getInstance();
+
+    void endInstance();
+
+    void showFeedbackLabel(QString output);
+
+    void showFeedbackInputEdit(QString output);
+
+    void showFocusInInputEdit (bool focus);
+
+    void showAppropriateColorInputEdit (InputBarFlag color);
+
+    void showTableResults(QVector <QString> output);
+
+    void resetTableContents();
+
+    void showNoTableDisplay();
+
+    void showTodayEvents();
+
+    void show();
+
+    void hide();
+
+    bool interfaceCurrentlyChanging();
+
+signals:
+    void relay(QString input);
+
+    void run (QString input, bool focus);
+
+    void toSeampleView(QString input, QString inputChecked, bool focus);
+
+private slots:
+    void recieve(QString input);
+
+    void enterTriggered();
+
+    void changeViewTriggered();
+
+    void undoTriggered();
+
+    void redoTriggered();
+
+    void addTriggered();
+
+    void findTriggered();
+
+    void displayTriggered();
+
+    void deleteTriggered();
+
+    void editTriggered();
+
+    void changeWorkingTabTriggered();
+
+    void clearTriggered();
+
+    void fadeInChange();
+
+    void fadeOutChange();
+
+private:    
+    bool singleInstanceExists();
+
+    void addTableContent(TableListNode *curr);
+
+    void changeGeometry();
+
+    int getPosX(int maxX);
+
+    int getPosY(int maxY);
+
+    void setTableParam();
+
+    void setSignals();
+
+private:
+    double opacityLvl;
+
+    QTimer fadeInTimer;
+
+    QTimer fadeOutTimer;
+
+    Ui::StandardView *ui;
+
+    TableListNode *_tail;
+};
+
+#endif // STANDARDVIEW_H
