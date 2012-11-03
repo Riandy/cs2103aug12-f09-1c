2c1ec9a2f2560ad1c0048241856d3954e04f4994
samsung <samsung@WY.comp.nus.edu.sg>
2012-11-03 10:17:44 +0800

    Gui Update - Update files

---
+++ SeamPLE/task.h
@@ -0,0 +1,61 @@
+#ifndef TASK_H
+#define TASK_H
+#include <string>
+#include <ctime>
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+
+class task
+{
+private:
+    string _description;
+    tm _startDate;
+    tm _endDate;
+    string _priority;
+    string _category;
+    int dateType;
+    int _id;
+
+
+
+public:
+
+    const static int DATENORMAL;
+    const static int DATEWEEKLY;
+    const static int DATEFORTNIGHTLY;
+    const static int DATEMONTHLY;
+
+    task();
+    ~task();
+
+    string getEventName();
+    void setEventName(string);
+
+    tm getStartDate();
+    void setStartDate( tm);
+    void setStartDateWithoutTime(tm);
+
+    tm getEndDate();
+    void setEndDate( tm);
+
+    string getPriority();
+    void setPriority(string);
+
+    string getCategory();
+    void setCategory(string);
+
+    int getID();
+    void setID(int);
+
+    int getDateType();
+    void setDateType(int );
+
+    string convertToDate(tm);
+
+    vector<string> toString();
+    static tm getEmptyDateTm();
+};
+#endif
