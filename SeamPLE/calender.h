2ff4f31dbdc5af22fbc34ae9c5be6ae0f93fbf88
Riandy <Riandy@Riandy-PC>
2012-11-04 12:30:47 +0800

    fixed minor sorting bug

---
+++ SeamPLE/calender.h
@@ -0,0 +1,84 @@
+#ifndef CALENDER_H
+#define CALENDER_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stdlib.h>
+#include <ctime>
+#include <sstream>
+#include "task.h"
+#include <stack>
+#include "ndebug.h"
+#include "ErrorLogger.h"
+using namespace std;
+
+
+class calender
+{
+public:
+
+private:
+
+
+    vector<task> _storage;
+    ErrorLogger* _faulty;
+
+    stack<string> _commandHistory;
+    stack<string> _undoHistory;
+
+    //used in add
+    stack<string> _addHistory;
+    stack<task> _undoneAddTasks;
+    // used in delete
+     stack <task> _deletedTasks;
+     stack<string> _deleteHistory;
+    //used in edit
+    stack <task> _undoOriginalEdits;
+    stack<task> _redoNewEdits;
+    stack<task> _redoOriginalEdits;
+    stack <task> _undoNewEdits;
+
+
+    void saveDeletedTask(int taskID);
+    void saveHistory(string command);
+    void undoOriginalEdits(task _oldtask);
+    void undoNewEdits(task _newtask);
+    void redoOriginalEdits(task _oldtask);
+    void redoNewEdits(task _newtask);
+    void saveAdd(string Item);
+    void saveUndoneDelete(string Item);
+
+    bool writeFile();
+    bool loadFile(char* fileName);
+    void writeBackupFile();
+    bool checkNameExists(string _name);
+
+   string ensureUniqueName(string _name);
+public:
+
+    calender();
+    ~calender();
+    int getTaskID(string searchItem);
+    bool addItem(task currentTask);
+    bool deleteItem(int taskID);
+    bool deleteItem(string eventName);
+    bool checkID(int id);
+    bool editTask(task _edited);
+    bool undoAction();
+    bool redoAction();
+    vector<task> SearchByCat(string searchItem);
+    vector<task> SearchByTask(string searchItem);
+    vector<task> SearchByPartialTask(string searchItem);
+    task* pointerSearchByTask(string searchItem);
+    vector<task> SearchByDate(string todayDate);
+    vector<task> displayDatabase();
+    vector<task> getToday();
+    vector<task> SortByEvent();
+    void SortByDate();
+    vector<task> SortByCategory();
+    string convertToDate(tm _date);
+    static bool dateComparator(task task1,task task2);
+};
+#endif
