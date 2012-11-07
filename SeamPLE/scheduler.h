1a97d8683c6479e3ad9c2adf57bbf0c56946e41f
Riandy <Riandy@Riandy-PC>
2012-11-07 22:13:33 +0800

    changed the date return to be "-" when the task is floating instead of 0:0:0

---
+++ SeamPLE/scheduler.h
@@ -0,0 +1,46 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+#include "calender.h"
+#include <ctime>
+#include <sstream>
+#include <algorithm>
+#include "ndebug.h"
+#include "Action.h"
+#include "ErrorLogger.h"
+
+
+
+class scheduler
+{
+private:
+    ErrorLogger* _faulty;
+    static scheduler *_scheduler;
+    scheduler();
+    vector<string> _result;
+    vector<task> taskVector;
+    calender eventCalender;
+    void updateGUI();
+    void updateResultFound(int size);
+    string convertToDate(tm _date);
+    task processAction(Action newAction);
+    void updateTask(task &_task);
+    void updateWeeklyTask(tm &_date);
+    void updateFornightlyTask(tm &_date);
+    void updateMonthlyTask(tm &_date);
+    int daysMonth(int year, int month);
+    void printMessage(string _messageType);
+    void partialUpdateGUI(vector<task> taskVector);
+    bool isFloatingTask(task task1);
+
+
+public:
+    static bool instanceFlag;
+    static scheduler* getInstance();
+    ~scheduler();
+    vector<string> executeCommand(Action newaction);
+    string getEventBasedOnTime(int hour, int min);
+
+};
+
+
+#endif
---
+++ SeamPLE/seample.h
@@ -0,0 +1,51 @@
+#ifndef SEAMPLE_H
+#define SEAMPLE_H
+#include <QtGui/QApplication>
+
+#include "Intellisense.h"
+#include "Action.h"
+#include <QDebug>
+#include <string>
+#include <iostream>
+#include "seample.h"
+#include "scheduler.h"
+
+
+
+enum Command
+{
+    TO_INTELLISENSE,
+    TO_SCHEDULER_AND_RETURN_RESULTS,
+    TO_SCHEDULER_AND_RETURN_TODAY_EVENTS
+};
+
+
+class Seample
+{
+
+private:
+    static bool instanceFlag;
+    static Seample *seample;
+    Seample();
+    string userInput;
+    Action response;
+    Intellisense *intellisense;
+    scheduler  *_scheduler;
+    QVector <QString> feedback;
+
+    //Following function is for converting vector of QString
+    //to QVector of QString
+    QVector <QString> convertQString (vector <string> buffer);
+
+    void setShortCutRequirementsMet(string);
+
+public:
+    static Seample* getInstance();
+    ~Seample();
+    void init(int argc, char *argv[]);
+    void updateUserInput(string userInput);
+    QVector <QString> run(bool runCommand, string _userInput);
+    QVector <QString> fireAction();
+};
+
+#endif // SEAMPLE_H
