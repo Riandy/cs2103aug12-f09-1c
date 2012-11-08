653925b3edb710a92ca91037d8af90566f52bea1
Beans_admin <Beans_admin@Beans_admin-PC.nus.edu.sg>
2012-11-09 01:18:09 +0800

    intellisense now able to accept timeformat X:XX

--- SeamPLE/intellisense.h
+++ SeamPLE/intellisense.h
@@ -212,6 +212,7 @@ private:
     bool processTimeFormat5 (tm &date, string time);
     bool processTimeFormat6 (tm &date, string time);
     bool processTimeFormat7 (tm &date, string time);
+    bool processTimeFormat8 (tm &date, string time);
 
     bool checkImptDate(vector<string>& tokens,tm &date);
 
