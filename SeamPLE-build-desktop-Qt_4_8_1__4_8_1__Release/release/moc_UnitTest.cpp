c56c7a72cbe0fdae27f70d5b1d82f764396801ec
pwr <a0083711@nus.edu.sg>
2012-11-12 11:49:52 +0800

    added build folder

---
+++ SeamPLE-build-desktop-Qt_4_8_1__4_8_1__Release/release/moc_UnitTest.cpp
@@ -0,0 +1,311 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'UnitTest.h'
+**
+** Created: Sun 11. Nov 20:12:54 2012
+**      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../SeamPLE/UnitTest.h"
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'UnitTest.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 63
+#error "This file was generated using the moc from 4.8.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+static const uint qt_meta_data_UnitTest[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      89,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: signature, parameters, type, tag, flags
+      10,    9,    9,    9, 0x08,
+      22,    9,    9,    9, 0x08,
+      35,    9,    9,    9, 0x08,
+      57,    9,    9,    9, 0x08,
+      80,    9,    9,    9, 0x08,
+     103,    9,    9,    9, 0x08,
+     126,    9,    9,    9, 0x08,
+     149,    9,    9,    9, 0x08,
+     171,    9,    9,    9, 0x08,
+     194,    9,    9,    9, 0x08,
+     217,    9,    9,    9, 0x08,
+     240,    9,    9,    9, 0x08,
+     264,    9,    9,    9, 0x08,
+     287,    9,    9,    9, 0x08,
+     313,    9,    9,    9, 0x08,
+     336,    9,    9,    9, 0x08,
+     363,    9,    9,    9, 0x08,
+     391,    9,    9,    9, 0x08,
+     414,    9,    9,    9, 0x08,
+     437,    9,    9,    9, 0x08,
+     460,    9,    9,    9, 0x08,
+     483,    9,    9,    9, 0x08,
+     502,    9,    9,    9, 0x08,
+     522,    9,    9,    9, 0x08,
+     542,    9,    9,    9, 0x08,
+     562,    9,    9,    9, 0x08,
+     582,    9,    9,    9, 0x08,
+     593,    9,    9,    9, 0x08,
+     605,    9,    9,    9, 0x08,
+     617,    9,    9,    9, 0x08,
+     629,    9,    9,    9, 0x08,
+     640,    9,    9,    9, 0x08,
+     652,    9,    9,    9, 0x08,
+     664,    9,    9,    9, 0x08,
+     676,    9,    9,    9, 0x08,
+     688,    9,    9,    9, 0x08,
+     700,    9,    9,    9, 0x08,
+     722,    9,    9,    9, 0x08,
+     745,    9,    9,    9, 0x08,
+     768,    9,    9,    9, 0x08,
+     793,    9,    9,    9, 0x08,
+     816,    9,    9,    9, 0x08,
+     828,    9,    9,    9, 0x08,
+     840,    9,    9,    9, 0x08,
+     852,    9,    9,    9, 0x08,
+     864,    9,    9,    9, 0x08,
+     876,    9,    9,    9, 0x08,
+     888,    9,    9,    9, 0x08,
+     900,    9,    9,    9, 0x08,
+     912,    9,    9,    9, 0x08,
+     925,    9,    9,    9, 0x08,
+     938,    9,    9,    9, 0x08,
+     968,    9,    9,    9, 0x08,
+     999,    9,    9,    9, 0x08,
+    1030,    9,    9,    9, 0x08,
+    1061,    9,    9,    9, 0x08,
+    1072,    9,    9,    9, 0x08,
+    1084,    9,    9,    9, 0x08,
+    1096,    9,    9,    9, 0x08,
+    1108,    9,    9,    9, 0x08,
+    1120,    9,    9,    9, 0x08,
+    1133,    9,    9,    9, 0x08,
+    1146,    9,    9,    9, 0x08,
+    1159,    9,    9,    9, 0x08,
+    1172,    9,    9,    9, 0x08,
+    1185,    9,    9,    9, 0x08,
+    1198,    9,    9,    9, 0x08,
+    1211,    9,    9,    9, 0x08,
+    1224,    9,    9,    9, 0x08,
+    1237,    9,    9,    9, 0x08,
+    1250,    9,    9,    9, 0x08,
+    1261,    9,    9,    9, 0x08,
+    1273,    9,    9,    9, 0x08,
+    1285,    9,    9,    9, 0x08,
+    1297,    9,    9,    9, 0x08,
+    1309,    9,    9,    9, 0x08,
+    1323,    9,    9,    9, 0x08,
+    1344,    9,    9,    9, 0x08,
+    1372,    9,    9,    9, 0x08,
+    1401,    9,    9,    9, 0x08,
+    1415,    9,    9,    9, 0x08,
+    1430,    9,    9,    9, 0x08,
+    1446,    9,    9,    9, 0x08,
+    1464,    9,    9,    9, 0x08,
+    1483,    9,    9,    9, 0x08,
+    1502,    9,    9,    9, 0x08,
+    1522,    9,    9,    9, 0x08,
+    1541,    9,    9,    9, 0x08,
+    1561,    9,    9,    9, 0x08,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_UnitTest[] = {
+    "UnitTest\0\0dummyTest()\0dummyTest1()\0"
+    "testIntellisenseAdd()\0testIntellisenseAdd2()\0"
+    "testIntellisenseAdd3()\0testIntellisenseAdd4()\0"
+    "testIntellisenseAdd5()\0testIntellisenseDel()\0"
+    "testIntellisenseDel2()\0testIntellisenseDEL3()\0"
+    "testIntellisenseEdit()\0testIntellisenseEdit2()\0"
+    "testIntellisenseExit()\0testIntellisenseDisplay()\0"
+    "testIntellisenseMark()\0"
+    "testIntellisenseQuickAdd()\0"
+    "testIntellisenseQuickAdd2()\0"
+    "testIntellisenseSort()\0testIntellisenseFind()\0"
+    "testIntellisenseUndo()\0testIntellisenseRedo()\0"
+    "testSchedulerAdd()\0testSchedulerAdd2()\0"
+    "testSchedulerAdd3()\0testSchedulerAdd4()\0"
+    "testSchedulerAdd5()\0testUndo()\0"
+    "testUndo2()\0testUndo3()\0testUndo4()\0"
+    "testRedo()\0testRedo2()\0testRedo3()\0"
+    "testRedo4()\0testUndo5()\0testRedo5()\0"
+    "testSchedulerDelete()\0testSchedulerDelete2()\0"
+    "testSchedulerDelete3()\0testSchedulerDelete3_2()\0"
+    "testSchedulerDelete4()\0testUndo6()\0"
+    "testUndo7()\0testUndo8()\0testUndo9()\0"
+    "testRedo6()\0testRedo7()\0testRedo8()\0"
+    "testRedo9()\0testUndo10()\0testRedo10()\0"
+    "testSchedulerUnknownCommand()\0"
+    "testSchedulerUnknownCommand2()\0"
+    "testSchedulerUnknownCommand3()\0"
+    "testSchedulerUnknownCommand4()\0"
+    "testEdit()\0testEdit2()\0testEdit3()\0"
+    "testEdit4()\0testEdit5()\0testUndo11()\0"
+    "testUndo12()\0testUndo13()\0testUndo14()\0"
+    "testRedo11()\0testRedo12()\0testRedo13()\0"
+    "testRedo14()\0testUndo15()\0testRedo15()\0"
+    "testMark()\0testMark2()\0testMark3()\0"
+    "testMark4()\0testMark5()\0testAddItem()\0"
+    "testDeleteItemByID()\0testDeleteItemByEventName()\0"
+    "testDeleteItemByEventName2()\0testcheckID()\0"
+    "testcheckID2()\0testdeleteAll()\0"
+    "testsearchByCat()\0testsearchByCat2()\0"
+    "testsearchByTask()\0testsearchByTask2()\0"
+    "testsearchByDate()\0testconvertToDate()\0"
+    "testconvertToDateWithoutTime()\0"
+};
+
+void UnitTest::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        UnitTest *_t = static_cast<UnitTest *>(_o);
+        switch (_id) {
+        case 0: _t->dummyTest(); break;
+        case 1: _t->dummyTest1(); break;
+        case 2: _t->testIntellisenseAdd(); break;
+        case 3: _t->testIntellisenseAdd2(); break;
+        case 4: _t->testIntellisenseAdd3(); break;
+        case 5: _t->testIntellisenseAdd4(); break;
+        case 6: _t->testIntellisenseAdd5(); break;
+        case 7: _t->testIntellisenseDel(); break;
+        case 8: _t->testIntellisenseDel2(); break;
+        case 9: _t->testIntellisenseDEL3(); break;
+        case 10: _t->testIntellisenseEdit(); break;
+        case 11: _t->testIntellisenseEdit2(); break;
+        case 12: _t->testIntellisenseExit(); break;
+        case 13: _t->testIntellisenseDisplay(); break;
+        case 14: _t->testIntellisenseMark(); break;
+        case 15: _t->testIntellisenseQuickAdd(); break;
+        case 16: _t->testIntellisenseQuickAdd2(); break;
+        case 17: _t->testIntellisenseSort(); break;
+        case 18: _t->testIntellisenseFind(); break;
+        case 19: _t->testIntellisenseUndo(); break;
+        case 20: _t->testIntellisenseRedo(); break;
+        case 21: _t->testSchedulerAdd(); break;
+        case 22: _t->testSchedulerAdd2(); break;
+        case 23: _t->testSchedulerAdd3(); break;
+        case 24: _t->testSchedulerAdd4(); break;
+        case 25: _t->testSchedulerAdd5(); break;
+        case 26: _t->testUndo(); break;
+        case 27: _t->testUndo2(); break;
+        case 28: _t->testUndo3(); break;
+        case 29: _t->testUndo4(); break;
+        case 30: _t->testRedo(); break;
+        case 31: _t->testRedo2(); break;
+        case 32: _t->testRedo3(); break;
+        case 33: _t->testRedo4(); break;
+        case 34: _t->testUndo5(); break;
+        case 35: _t->testRedo5(); break;
+        case 36: _t->testSchedulerDelete(); break;
+        case 37: _t->testSchedulerDelete2(); break;
+        case 38: _t->testSchedulerDelete3(); break;
+        case 39: _t->testSchedulerDelete3_2(); break;
+        case 40: _t->testSchedulerDelete4(); break;
+        case 41: _t->testUndo6(); break;
+        case 42: _t->testUndo7(); break;
+        case 43: _t->testUndo8(); break;
+        case 44: _t->testUndo9(); break;
+        case 45: _t->testRedo6(); break;
+        case 46: _t->testRedo7(); break;
+        case 47: _t->testRedo8(); break;
+        case 48: _t->testRedo9(); break;
+        case 49: _t->testUndo10(); break;
+        case 50: _t->testRedo10(); break;
+        case 51: _t->testSchedulerUnknownCommand(); break;
+        case 52: _t->testSchedulerUnknownCommand2(); break;
+        case 53: _t->testSchedulerUnknownCommand3(); break;
+        case 54: _t->testSchedulerUnknownCommand4(); break;
+        case 55: _t->testEdit(); break;
+        case 56: _t->testEdit2(); break;
+        case 57: _t->testEdit3(); break;
+        case 58: _t->testEdit4(); break;
+        case 59: _t->testEdit5(); break;
+        case 60: _t->testUndo11(); break;
+        case 61: _t->testUndo12(); break;
+        case 62: _t->testUndo13(); break;
+        case 63: _t->testUndo14(); break;
+        case 64: _t->testRedo11(); break;
+        case 65: _t->testRedo12(); break;
+        case 66: _t->testRedo13(); break;
+        case 67: _t->testRedo14(); break;
+        case 68: _t->testUndo15(); break;
+        case 69: _t->testRedo15(); break;
+        case 70: _t->testMark(); break;
+        case 71: _t->testMark2(); break;
+        case 72: _t->testMark3(); break;
+        case 73: _t->testMark4(); break;
+        case 74: _t->testMark5(); break;
+        case 75: _t->testAddItem(); break;
+        case 76: _t->testDeleteItemByID(); break;
+        case 77: _t->testDeleteItemByEventName(); break;
+        case 78: _t->testDeleteItemByEventName2(); break;
+        case 79: _t->testcheckID(); break;
+        case 80: _t->testcheckID2(); break;
+        case 81: _t->testdeleteAll(); break;
+        case 82: _t->testsearchByCat(); break;
+        case 83: _t->testsearchByCat2(); break;
+        case 84: _t->testsearchByTask(); break;
+        case 85: _t->testsearchByTask2(); break;
+        case 86: _t->testsearchByDate(); break;
+        case 87: _t->testconvertToDate(); break;
+        case 88: _t->testconvertToDateWithoutTime(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObjectExtraData UnitTest::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject UnitTest::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_UnitTest,
+      qt_meta_data_UnitTest, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &UnitTest::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *UnitTest::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *UnitTest::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_UnitTest))
+        return static_cast<void*>(const_cast< UnitTest*>(this));
+    return QObject::qt_metacast(_clname);
+}
+
+int UnitTest::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 89)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 89;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
